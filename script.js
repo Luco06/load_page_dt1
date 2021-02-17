468f18f2dc12125702b240d7e72a8b8ee87b97c7
Luc-olivier <42836253+Luco06@users.noreply.github.com>
2021-02-17 16:52:32 +0100

    last commit
    Add progress bar and animation

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const anime = document.getElementById('anime');
+
+function progress(){
+    var i =0;
+    var run = setInterval(function frames(){
+        i = i +1;
+        if(i == 101){
+            clearInterval(run);
+        }else{
+            var counter  = document.querySelector(".counter");
+            counter.textContent = i + "mg/dl";
+        }
+    },25)
+    setTimeout(function stopPoint(){
+        var stop = document.querySelector(".stop");
+        stop.style.display = "block";
+    },5000);
+    }
+
 anime.addEventListener('click', event => {
     var finger = document.getElementById('finger');
     var tear = document.getElementById('tear');
+    var box = document.getElementById('box');
     finger.style.transitionDuration="1s";
     finger.style.opacity--;
 
     tear.style.visibility="visible";
-    tear.style.transform="translateY(235px)";
-    tear.style.opacity--;
+    tear.style.transform="translateY(200px)";
     tear.style.transitionDuration="3s";
-
+    tear.style.opacity--;
+    
+    box.style.visibility="visible";
+    progress();
 })
+
